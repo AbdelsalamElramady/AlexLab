63da3f48b6284276260fc12fcba8fab477f3c475
Abdelsalam Elramady <abdoelramady4@gmail.com>
2018-07-24 18:23:35 +0200

    Add room table to database and room class to BL

---
+++ AlexLabBL/MainClass.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AlexLabBL
+{
+    public class MainClass
+    {
+        public static String ConectionString { get; set; }
+        public static DataClassesAlexLabDataContext DbObj { get; set; }
+
+        public static Object getRooms()
+        {
+            DbObj = new DataClassesAlexLabDataContext(ConectionString);
+
+            var obj = from ob in DbObj.Rooms select ob;
+            return obj;
+        }
+    }
+}
---
+++ AlexLabBL/RoomClass.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AlexLabBL
+{
+    class RoomClass
+    {
+        public int RoomId { get; set; }
+        public string RoomDesc { get; set; }
+        public decimal HourPrice { get; set; }
+        public string Notes { get; set; }
+
+        public RoomClass()
+        {
+            MainClass.DbObj = new DataClassesAlexLabDataContext(MainClass.ConectionString);
+        }
+
+        public RoomClass(int RoomId)
+        {
+            MainClass.DbObj = new DataClassesAlexLabDataContext(MainClass.ConectionString);
+
+            var Room = (from r in MainClass.DbObj.Rooms where r.RoomId == RoomId select r).First();
+
+            RoomId = Room.RoomId;
+            RoomDesc = Room.RoomDesc;
+            HourPrice = Room.HourPrice.Value;
+            Notes = Room.Notes;
+        }
+
+        private bool Insert()
+        {
+            try
+            {
+                Room roo = new Room();
+
+                roo.RoomDesc = RoomDesc;
+                roo.HourPrice = HourPrice;
+                roo.Notes = Notes;
+
+                MainClass.DbObj.Rooms.InsertOnSubmit(roo);
+                MainClass.DbObj.SubmitChanges();
+
+                RoomId = roo.RoomId;
+
+                return true;
+            }
+            catch
+            {
+                return false;
+            }
+        }
+
+        private bool Update()
+        {
+            try
+            {
+                var roo = (from c in MainClass.DbObj.Rooms where c.RoomId == RoomId select c).First();
+
+                roo.RoomDesc = RoomDesc;
+                roo.HourPrice = HourPrice;
+                roo.Notes = Notes;
+
+                MainClass.DbObj.SubmitChanges();
+
+                return true;
+            }
+            catch
+            {
+                return false;
+            }
+        }
+
+        public bool Save(bool isEdit)
+        {
+            if (isEdit == true)
+            {
+                return Update();
+            }
+            else
+            {
+                return Insert();
+            }
+        }
+    }
+}
